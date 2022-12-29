2a19d833596ada385de648f06342f2e704638568
Daurius Swarts <ali@openmindi.co.za>
2022-12-29 12:59:20 +0200

    Update

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,129 +1,137 @@
 import {
-    MediaRenderer,
-    useAddress,
-    useContract,
-    useOwnedNFTs,
+  MediaRenderer,
+  useAddress,
+  useContract,
+  useOwnedNFTs,
 } from "@thirdweb-dev/react";
 import { NFT } from "@thirdweb-dev/sdk";
 import React from "react";
 import { ListingSkeleton, ListingCard } from ".";
 
 type Props = {
-    selectedNft: NFT | null;
-    setSelectedNft: React.Dispatch<React.SetStateAction<NFT | null>>;
-    setListingType: React.Dispatch<
-        React.SetStateAction<"directListing" | "auctionListing" | null>
-    >;
-    setPrice: React.Dispatch<React.SetStateAction<string>>;
-    nfts: NFT[] | undefined;
-    isLoadingNft: boolean;
-    refetchNtfs: () => void;
+  selectedNft: NFT | null;
+  setSelectedNft: React.Dispatch<
+    React.SetStateAction<NFT | null>
+  >;
+  setListingType: React.Dispatch<
+    React.SetStateAction<
+      "directListing" | "auctionListing" | null
+    >
+  >;
+  setPrice: React.Dispatch<
+    React.SetStateAction<string>
+  >;
+  nfts: NFT[] | undefined;
+  isLoadingNft: boolean;
+  refetchNtfs: () => void;
 };
 
 function ListItem({
-    selectedNft,
-    setSelectedNft,
-    setPrice,
-    setListingType,
-    nfts,
-    isLoadingNft,
-    refetchNtfs,
+  selectedNft,
+  setSelectedNft,
+  setPrice,
+  setListingType,
+  nfts,
+  isLoadingNft,
+  refetchNtfs,
 }: Props) {
-    React.useEffect(() => {
-        refetchNtfs();
-    }, []);
-    return (
-        <main className="overflow-hidden">
-            <h5 className="font-medium text-xl">
-                Select a Property for sale as NFT.
-            </h5>
-            <p className="text-xs dark:text-gray-300">
-                Here you will find Property NFT's you have uploaded and are
-                linked to your wallet
-            </p>
+  React.useEffect(() => {
+    refetchNtfs();
+  }, []);
+  return (
+    <main className="overflow-hidden">
+      <h5 className="font-medium text-xl">
+        Select a Property for sale as NFT.
+      </h5>
+      <p className="text-xs dark:text-gray-300">
+        Here you will find Property NFT's you have
+        uploaded and are linked to your wallet
+      </p>
 
-            <div className="p-5 py-8 overflow-x-auto scrollbar-thin scrollbar-thumb-[#0EA7E6] scrollbar-track-[#2c2b2b]">
-                <div className="flex items-center space-x-6">
-                    {isLoadingNft ? (
-                        <>
-                            {/* Skeleton Loading */}
-                            {Array(5)
-                                .fill(0)
-                                .map((_, i) => (
-                                    <ListingSkeleton />
-                                ))}
-                        </>
-                    ) : (
-                        <>
-                            {nfts?.map((nft) => (
-                                <ListingCard
-                                    noHover
-                                    active={
-                                        selectedNft?.metadata.id ===
-                                        nft.metadata.id
-                                    }
-                                    key={nft.metadata.id}
-                                >
-                                    <div
-                                        onClick={() => {
-                                            if (
-                                                selectedNft &&
-                                                selectedNft?.metadata.id ===
-                                                    nft.metadata.id
-                                            ) {
-                                                setSelectedNft(null);
-                                            } else {
-                                                setSelectedNft(nft);
-                                            }
-                                        }}
-                                        className="flex flex-col bg-gray-100 -m-5 rounded-lg p-2 w-72"
-                                    >
-                                        <MediaRenderer
-                                            className="rounded-lg w-72"
-                                            src={nft.metadata.image}
-                                        />
-                                        <p className="truncate pt-2 text-base font-md text-[#080a0b]">
-                                            {
-                                                nft.metadata
-                                                    .typeOfProperty as string
-                                            }
-                                        </p>
+      <div className="p-5 py-8 overflow-x-auto scrollbar-thin scrollbar-thumb-[#0EA7E6] scrollbar-track-[#2c2b2b]">
+        <div className="flex items-center space-x-6">
+          {isLoadingNft ? (
+            <>
+              {/* Skeleton Loading */}
+              {Array(5)
+                .fill(0)
+                .map((_, i) => (
+                  <ListingSkeleton />
+                ))}
+            </>
+          ) : (
+            <>
+              {nfts?.map((nft) => (
+                <ListingCard
+                  noHover
+                  active={
+                    selectedNft?.metadata.id ===
+                    nft.metadata.id
+                  }
+                  key={nft.metadata.id}
+                >
+                  <div
+                    onClick={() => {
+                      if (
+                        selectedNft &&
+                        selectedNft?.metadata
+                          .id === nft.metadata.id
+                      ) {
+                        setSelectedNft(null);
+                      } else {
+                        setSelectedNft(nft);
+                      }
+                    }}
+                    className="flex flex-col bg-gray-100 items-start rounded-lg p-2 w-full"
+                  >
+                    <MediaRenderer
+                      className="rounded-lg w-72"
+                      src={nft.metadata.image}
+                    />
+                    <p className="truncate pt-2 text-base font-md text-[#080a0b]">
+                      {
+                        nft.metadata
+                          .typeOfProperty as string
+                      }
+                    </p>
 
-                                        <p className="truncate pb-2 text-xs font-md text-gray-400">
-                                            {nft.metadata.name}
-                                        </p>
-                                        <hr />
-                                        <p className="text-xs text-gray-400 pt-2">
-                                            {nft.metadata.description}
-                                        </p>
-                                    </div>
-                                </ListingCard>
-                            ))}
-                        </>
-                    )}
-                </div>
-            </div>
+                    <p className="truncate pb-2 text-xs font-md text-gray-400">
+                      {nft.metadata.name}
+                    </p>
+                    <hr />
+                    <p className="text-xs text-gray-400 pt-2">
+                      {nft.metadata.description}
+                    </p>
+                  </div>
+                </ListingCard>
+              ))}
+            </>
+          )}
+        </div>
+      </div>
 
-            {selectedNft && (
-                <form>
-                    <div className="flex flex-col p-10">
-                        <div className="grid grid-cols-2 gap-5">
-                            <label
-                                htmlFor="directListing"
-                                className="border-r font-light"
-                            >
-                                Direct Listing
-                            </label>
-                            <input
-                                type="radio"
-                                name="listingType"
-                                id="directListing"
-                                value="directListing"
-                                className="ml-auto h-10 w-10 cursor-pointer"
-                                onChange={() => setListingType("directListing")}
-                            />
+      {selectedNft && (
+        <form>
+          <div className="flex flex-col p-10">
+            <div className="grid grid-cols-2 gap-5">
+              <label
+                htmlFor="directListing"
+                className="border-r font-light"
+              >
+                Direct Listing
+              </label>
+              <input
+                type="radio"
+                name="listingType"
+                id="directListing"
+                value="directListing"
+                className="ml-auto h-10 w-10 cursor-pointer"
+                onChange={() =>
+                  setListingType("directListing")
+                }
+              />
 
-                            {/* <label
+              {/* <label
                 htmlFor="auctionListing"
                 className="border-r font-light"
               >
@@ -140,24 +148,26 @@ function ListItem({
                 }
               /> */}
 
-                            <label
-                                htmlFor="price"
-                                className="border-r font-light"
-                            >
-                                Price
-                            </label>
-                            <input
-                                type="text"
-                                placeholder="0.05"
-                                className="outline-none bg-gray-100 p-3 border rounded-md"
-                                onChange={(e) => setPrice(e.target.value)}
-                            />
-                        </div>
-                    </div>
-                </form>
-            )}
-        </main>
-    );
+              <label
+                htmlFor="price"
+                className="border-r font-light"
+              >
+                Price
+              </label>
+              <input
+                type="text"
+                placeholder="0.05"
+                className="outline-none bg-gray-100 text-gray-500 dark:text-gray-400 p-3 border rounded-md"
+                onChange={(e) =>
+                  setPrice(e.target.value)
+                }
+              />
+            </div>
+          </div>
+        </form>
+      )}
+    </main>
+  );
 }
 
 export default ListItem;
